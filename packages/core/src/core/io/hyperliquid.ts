66241f174d0a3af3a3d453276533ebb57b8bfa95
caillef <corentin.cailleaud@caillef.com>
2025-01-28 19:14:14 +0100

    feat: add hyperliquid market/limit order

---
+++ packages/core/src/core/io/hyperliquid.ts
@@ -0,0 +1,165 @@
+import { Hyperliquid } from "hyperliquid";
+import { Logger } from "../../core/logger";
+import { LogLevel } from "../types";
+
+export interface HyperliquidCredentials {
+    mainAddress: string;
+    walletAddress: string;
+    privateKey: string;
+}
+
+export class HyperliquidClient {
+    private client: Hyperliquid;
+    private mainAddress: string;
+    private logger: Logger;
+    private perpMeta: any;
+
+    constructor(
+        credentials: HyperliquidCredentials,
+        logLevel: LogLevel = LogLevel.INFO,
+        testnet: boolean = false
+    ) {
+        this.mainAddress = credentials.mainAddress;
+        this.client = new Hyperliquid({
+            enableWs: true,
+            privateKey: credentials.privateKey,
+            testnet,
+            walletAddress: credentials.walletAddress,
+        });
+        this.logger = new Logger({
+            level: logLevel,
+            enableColors: true,
+            enableTimestamp: true,
+        });
+
+        this.client.connect().catch((error) => {
+            this.logger.error("HyperliquidClient", "Failed to login", {
+                error,
+            });
+        });
+    }
+
+    public async placeLimitOrderInstantOrCancel(
+        ticker: string,
+        sz: number,
+        limit_px: number,
+        is_buy: boolean
+    ) {
+        return this.placeOrder(ticker, sz, limit_px, is_buy, {
+            limit: { tif: "Ioc" },
+        });
+    }
+
+    public async placeLimitOrderGoodTilCancel(
+        ticker: string,
+        sz: number,
+        limit_px: number,
+        is_buy: boolean
+    ) {
+        return this.placeOrder(ticker, sz, limit_px, is_buy, {
+            limit: { tif: "Gtc" },
+        });
+    }
+
+    public async getPositions() {
+        return (
+            await this.client.info.perpetuals.getClearinghouseState(
+                this.mainAddress
+            )
+        ).assetPositions;
+    }
+
+    public async marketSellPositions(tickers: string[]) {
+        const positions = await this.getPositions();
+        return Promise.all(
+            tickers.map((ticker) => this.marketSellPosition(ticker, positions))
+        );
+    }
+
+    public async marketSellPosition(ticker: string, positions: any) {
+        if (positions === undefined) {
+            positions = await this.getPositions();
+        }
+        const positionsMatching = positions.filter(
+            (obj: any) => obj.position.coin === ticker.toUpperCase() + "-PERP"
+        );
+        if (positionsMatching.length > 0) {
+            const size: number = Number(positionsMatching[0].position.szi);
+            return await this.placeMarketOrder(
+                ticker.toUpperCase(),
+                size,
+                false
+            );
+        }
+    }
+
+    public async placeMarketOrderUSD(
+        ticker: string,
+        totalprice: number,
+        is_buy: boolean
+    ) {
+        const orderbook = await this.client.info.getL2Book(ticker + "-PERP");
+        const triggerPx = is_buy
+            ? Number(orderbook.levels[1][3].px)
+            : Number(orderbook.levels[0][3].px);
+        if (!this.perpMeta) {
+            // Save szDecimals once
+            this.perpMeta = {};
+            (await this.client.info.perpetuals.getMeta()).universe.forEach(
+                (token: { name: string; szDecimals: number }) => {
+                    this.perpMeta[token.name] = token.szDecimals;
+                }
+            );
+        }
+        let szDecimals = this.perpMeta[ticker + "-PERP"];
+        if (szDecimals === undefined) {
+            throw new Error("Can't find szDecimals for " + ticker);
+        }
+        const sz = Number((totalprice / triggerPx).toFixed(szDecimals));
+        const result = await this.placeOrder(ticker, sz, triggerPx, is_buy, {
+            limit: { tif: "Ioc" },
+        });
+        return result;
+    }
+
+    public async placeMarketOrder(ticker: string, sz: number, is_buy: boolean) {
+        const orderbook = await this.client.info.getL2Book(ticker + "-PERP");
+        const triggerPx = is_buy
+            ? Number(orderbook.levels[1][3].px)
+            : Number(orderbook.levels[0][3].px);
+        const result = await this.placeOrder(ticker, sz, triggerPx, is_buy, {
+            limit: { tif: "Ioc" },
+        });
+        return result;
+    }
+
+    private async placeOrder(
+        ticker: string,
+        sz: number,
+        limit_px: number,
+        is_buy: boolean,
+        order_type: object
+    ) {
+        return this.client.exchange.placeOrder({
+            coin: ticker + "-PERP",
+            is_buy,
+            sz,
+            limit_px,
+            order_type,
+            reduce_only: false,
+        });
+    }
+}
+
+// Example usage:
+/*
+const hyperliquid = new HyperliquidClient(
+    {
+        mainAddress: env.HYPERLIQUID_MAIN_ADDRESS,
+        walletAddress: env.HYPERLIQUID_WALLET_ADDRESS,
+        privateKey: env.HYPERLIQUID_PRIVATE_KEY,
+    },
+    loglevel
+);
+
+*/
