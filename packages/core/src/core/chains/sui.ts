e59fb5d49f0d6a3b5a0f195fe2b78de27004a078
0xrpj <proshan@skiff.com>
2025-02-04 22:43:53 +0545

    add more methods in sui

--- packages/core/src/core/chains/sui.ts
+++ packages/core/src/core/chains/sui.ts
@@ -1,10 +1,26 @@
-import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
+import { getFullnodeUrl, SuiClient, type SuiTransactionBlockResponse } from "@mysten/sui/client";
 import { getFaucetHost, requestSuiFromFaucetV0 } from '@mysten/sui/faucet';
 import type { IChain } from "../types";
 import type { Signer } from "@mysten/sui/cryptography";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import { Secp256k1Keypair } from "@mysten/sui/keypairs/secp256k1";
 import { Secp256r1Keypair } from "@mysten/sui/keypairs/secp256r1";
+import { Transaction, type TransactionObjectArgument } from "@mysten/sui/transactions";
+import { SUI_DECIMALS } from "@mysten/sui/utils";
+import { AggregatorClient, Env } from "@cetusprotocol/aggregator-sdk";
+import BN from "bignumber.js";
+
+interface SwapResult {
+    success: boolean;
+    tx: string;
+    message: string;
+}
+
+interface TokenMetadata {
+    symbol: string;
+    decimals: number;
+    tokenAddress: string;
+}
 
 const parseAccount = (privateKey: string): Signer => {
     if (privateKey.startsWith("suiprivkey")) {
@@ -39,6 +55,37 @@ const loadFromMnemonics = (mnemonics: string) => {
 
 export type SuiNetwork = "mainnet" | "testnet" | "devnet" | "localnet";
 export type FaucetNetwork = "testnet" | "devnet" | "localnet";
+
+export const supportedSuiTokens: Map<string, TokenMetadata> = new Map([
+    [
+        "SUI",
+        {
+            symbol: "SUI",
+            decimals: SUI_DECIMALS,
+            tokenAddress: "0x2::sui::SUI",
+        },
+    ],
+    [
+        "USDC",
+        {
+            symbol: "USDC",
+            decimals: 6,
+            tokenAddress:
+                "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC",
+        },
+    ],
+]);
+
+export const getTokenMetadata = (symbol: string) => {
+    const metadata = supportedSuiTokens.get(symbol.toUpperCase())
+    if (!metadata) throw new Error(`Token ${symbol} not found`);
+    return metadata;
+};
+
+export const getAmount = (amount: string, meta: TokenMetadata) => {
+    const v = parseFloat(amount);
+    return BigInt(v * 10 ** meta.decimals);
+};
 
 /**
  * Configuration options for initializing a Sui chain connection
@@ -85,6 +132,11 @@ export class SuiChain implements IChain {
             this.wallet = parseAccount(config.privateKey)
     }
 
+    /**
+     * Returns testnet sui tokens
+     * @param call - The params for faucet
+     * @returns Success/Failure message
+     */
     public async requestSui(call: unknown): Promise<any> {
         try {
             const { network, recipient } = call as {
@@ -108,13 +160,47 @@ export class SuiChain implements IChain {
     }
 
     /**
+     * Returns a transaction link based on network
+     * @param tx - The sui transaction
+     * @returns The the transaction link
+     */
+    public getTransactionLink(tx: string) {
+        if (this.network === "mainnet") {
+            return `https://suivision.xyz/txblock/${tx}`;
+        } else if (this.network === "testnet") {
+            return `https://testnet.suivision.xyz/txblock/${tx}`;
+        } else if (this.network === "devnet") {
+            return `https://devnet.suivision.xyz/txblock/${tx}`;
+        } else if (this.network === "localnet") {
+            return `localhost : ${tx}`;
+        }
+    }
+
+    /**
      * Performs a read-only call to a Sui contract
      * @param call - The contract call parameters
      * @returns The result of the contract call
      * @throws Error if the call fails
      */
-    public async read(call: any): Promise<any> {
-        // TODO
+    public async read(call: unknown): Promise<any> {
+        const { objectId } = call as {
+            objectId: string
+        }
+
+        const object = await this.client.getObject({
+            id: objectId,
+            options: {
+                showContent: true,
+                showBcs: true,
+                showDisplay: true,
+                showOwner: true,
+                showPreviousTransaction: true,
+                showStorageRebate: true,
+                showType: true
+            }
+        })
+
+        return object;
     }
 
     /**
@@ -123,7 +209,179 @@ export class SuiChain implements IChain {
      * @returns The transaction receipt after confirmation
      * @throws Error if the transaction fails
      */
-    public async write(call: any): Promise<any> {
-        // TODO
+    public async write(call: unknown): Promise<SuiTransactionBlockResponse> {
+        const { packageId, moduleName, functionName, params } = call as {
+            packageId: string,
+            moduleName: string,
+            functionName: string,
+            params: Array<any>
+        }
+
+        const tx = new Transaction();
+
+        tx.moveCall({
+            target: `${packageId}::${moduleName}::${functionName}`,
+            arguments: params,
+        });
+
+        const result = await this.client.signAndExecuteTransaction({
+            signer: this.wallet,
+            transaction: tx,
+        });
+
+        return result;
+    }
+
+    /**
+     * Executes a swap transaction on Sui
+     * @param call - The swap transaction parameters
+     * @returns The transaction digest and message after confirmation
+     * @throws Error if the transaction fails
+     */
+    public async swapToken(
+        call: unknown,
+    ): Promise<SwapResult> {
+        try {
+            const { fromToken, amount, out_min_amount, targetToken } = call as {
+                fromToken: string,
+                amount: string,
+                targetToken: string,
+                out_min_amount: number | null,
+            }
+
+            const aggregatorURL = "https://api-sui.cetus.zone/router_v2/find_routes";
+            const fromMeta = getTokenMetadata(fromToken);
+            const toMeta = getTokenMetadata(targetToken);
+            const client = new AggregatorClient(
+                aggregatorURL,
+                this.wallet.toSuiAddress(),
+                this.client,
+                Env.Mainnet
+            );
+            // provider list : https://api-sui.cetus.zone/router_v2/status
+            const routerRes = await client.findRouters({
+                from: fromMeta.tokenAddress,
+                target: toMeta.tokenAddress,
+                amount: new BN(amount),
+                byAmountIn: true, // `true` means fix input amount, `false` means fix output amount
+                depth: 3, // max allow 3, means 3 hops
+                providers: [
+                    "KRIYAV3",
+                    "CETUS",
+                    "SCALLOP",
+                    "KRIYA",
+                    "BLUEFIN",
+                    "DEEPBOOKV3",
+                    "FLOWXV3",
+                    "BLUEMOVE",
+                    "AFTERMATH",
+                    "FLOWX",
+                    "TURBOS",
+                    // "AFSUI",
+                    // "VOLO",
+                    // "SPRINGSUI",
+                    // "ALPHAFI",
+                    // "HAEDAL",
+                    // "HAEDALPMM",
+                ],
+            });
+            if (routerRes === null) {
+                console.error("No router found " + JSON.stringify({
+                    from: fromMeta.tokenAddress,
+                    target: toMeta.tokenAddress,
+                    amount: amount,
+                }))
+                return {
+                    success: false,
+                    tx: "",
+                    message: "No router found",
+                };
+            }
+
+            if (out_min_amount && routerRes.amountOut.toNumber() < out_min_amount) {
+                return {
+                    success: false,
+                    tx: "",
+                    message: "Out amount is less than out_min_amount",
+                };
+            }
+
+            let coin: TransactionObjectArgument;
+            const routerTx = new Transaction();
+
+            if (fromToken.toUpperCase() === "SUI") {
+                coin = routerTx.splitCoins(routerTx.gas, [amount]);
+            } else {
+                const allCoins = await this.client.getCoins({
+                    owner: this.wallet.toSuiAddress(),
+                    coinType: fromMeta.tokenAddress,
+                    limit: 30,
+                });
+
+                if (allCoins.data.length === 0) {
+                    console.error("No coins found");
+                    return {
+                        success: false,
+                        tx: "",
+                        message: "No coins found",
+                    };
+                }
+
+                const mergeCoins = [];
+
+                for (let i = 0; i < allCoins.data.length; i++) {
+                    mergeCoins.push(allCoins.data[i].coinObjectId);
+                }
+
+                routerTx.mergeCoins(allCoins.data[0].coinObjectId, mergeCoins);
+                coin = routerTx.splitCoins(allCoins.data[0].coinObjectId, [amount]);
+
+                console.log({ coinPing: coin })
+            }
+
+            const targetCoin = await client.routerSwap({
+                routers: routerRes!.routes,
+                byAmountIn: true,
+                txb: routerTx,
+                inputCoin: coin,
+                slippage: 0.5,
+            });
+
+            // checking threshold
+
+            // routerTx.moveCall({
+            //     package:
+            //         "0x57d4f00af225c487fd21eed6ee0d11510d04347ee209d2ab48d766e48973b1a4",
+            //     module: "utils",
+            //     function: "check_coin_threshold",
+            //     arguments: [
+            //         targetCoin,
+            //         routerTx.pure(bcs.U64.serialize(out_min_amount)),
+            //     ],
+            //     typeArguments: [otherType],
+            // });
+            routerTx.transferObjects([targetCoin], this.wallet.toSuiAddress());
+            routerTx.setSender(this.wallet.toSuiAddress());
+            const result = await client.signAndExecuteTransaction(
+                routerTx,
+                this.wallet
+            );
+
+            await this.client.waitForTransaction({
+                digest: result.digest,
+            });
+
+            return {
+                success: true,
+                tx: result.digest,
+                message: "Swap successful",
+            };
+        } catch (e) {
+            return {
+                success: false,
+                tx: 'n/a',
+                message: "Swap unsuccessful " + e?.toString(),
+            };
+        }
     }
 }
