7990b5739b5fef1a38a7af672cf2a778dfe7680d
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2025-02-10 14:55:27 +1100

    updates'

--- examples/v1/example-discord.ts
+++ examples/v1/example-discord.ts
@@ -7,9 +7,18 @@ import {
 } from "../../packages/core/src/core/v1/utils";
 import { DiscordClient } from "../../packages/core/src/core/v0/io/discord";
 import { createGroq } from "@ai-sdk/groq";
-import { LogLevel } from "../../packages/core/src/core/v1/types";
+import { InferMemoryFromHandler, LogLevel } from "../../packages/core/src/core/v1/types";
 import { tavilySearch } from "../../packages/core/src/core/v1/actions/tavily";
-import { createMemoryStore } from "../../packages/core/src/core/v1/memory";
+import {
+    createContextHandler,
+    createMemoryStore,
+    defaultContext,
+    defaultContextRender,
+    // getContextMemoryHandler,
+} from "@daydreamsai/core/src/core/v1/memory";
+
+import { Research } from "./deep-research/research";
+import { formatXml } from "@daydreamsai/core/src/core/v1/xml";
 
 const groq = createGroq({
     apiKey: process.env.GROQ_API_KEY!,
@@ -20,11 +29,35 @@ const discord = new DiscordClient({
     discord_bot_name: process.env.DISCORD_BOT_NAME!,
 }, LogLevel.DEBUG);
 
+
+const contextHandler = createContextHandler(
+    () => ({
+        ...defaultContext(),
+        researches: [] as Research[],
+    }),
+    (memory) => {
+        return [
+            ...defaultContextRender(memory),
+            ...memory.researches.map((r) =>
+                formatXml({
+                    tag: "research",
+                    params: { id: r.id },
+                    content: JSON.stringify(r),
+                })
+            ),
+        ];
+    }
+);
+
+type Handler = typeof contextHandler;
+type Memory = InferMemoryFromHandler<Handler>;
+
 async function main() {
-    createDreams({
+    const agent = createDreams<Memory, Handler>({
         logger: LogLevel.DEBUG,
-        model: groq("deepseek-r1-distill-llama-70b"),
         memory: createMemoryStore(),
+        context: contextHandler,
+        model: groq("deepseek-r1-distill-llama-70b"),
         inputs: {
             "discord:message": input({
                 schema: z.object({
@@ -73,7 +106,7 @@ async function main() {
 
         outputs: {
             "discord:message": output({
-                params: z.object({
+                schema: z.object({
                     channelId: z.string().describe("The Discord channel ID to send the message to"),
                     content: z.string().describe("The content of the message to send"),
                 }),
