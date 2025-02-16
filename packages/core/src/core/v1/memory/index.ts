9f2bfb31a97b062c0b1dea5410023e84339895fb
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2025-02-16 19:47:38 +1100

    vector

--- packages/core/src/core/v1/memory/index.ts
+++ packages/core/src/core/v1/memory/index.ts
@@ -1,18 +1,2 @@
-import type { MemoryStore, VectorStore } from "../types";
-
 export * from "./mongo";
 export * from "./base";
-
-export class BaseMemory {
-  store: MemoryStore;
-  vector: VectorStore;
-
-  constructor(store: MemoryStore, vector: VectorStore) {
-    this.store = store;
-    this.vector = vector;
-  }
-}
-
-export function createMemory(store: MemoryStore, vector: VectorStore) {
-  return new BaseMemory(store, vector);
-}
