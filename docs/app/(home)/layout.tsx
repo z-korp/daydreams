a7b91d5a8a4023bbb565a7e7ce0072214a4fa854
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2025-03-27 09:49:15 +1100

    docs

--- docs/app/(home)/layout.tsx
+++ docs/app/(home)/layout.tsx
@@ -1,7 +1,8 @@
 import type { ReactNode } from "react";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
-// import { baseOptions } from '@/app/layout.config';
+// @ts-ignore
+import { baseOptions } from "@/app/layout.config";
 
 export default function Layout({ children }: { children: any }) {
-  return <HomeLayout>{children}</HomeLayout>;
+  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
 }
