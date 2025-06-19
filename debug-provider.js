a7f8bc845ebf4ef562c38008cf63178949a6bfb2
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2025-06-19 16:21:40 -0500

    Ad

--- debug-provider.js
+++
@@ -1,7 +0,0 @@
-const { openrouter } = require("@openrouter/ai-sdk-provider");
-
-const model = openrouter("google/gemini-2.5-pro");
-console.log("Provider:", model.provider);
-console.log("Model ID:", model.modelId);
-console.log("Provider ID:", model.providerId);
-console.log("All properties:", Object.keys(model));
